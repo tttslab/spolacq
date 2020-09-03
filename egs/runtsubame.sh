245ef1d2a8ebaa276fd9910259426b9a55c0e4c1
KeisukeToyoda <keisuke.toyodano@gmail.com>
2020-09-03 20:58:21 +0900

    add runtsubame

---
+++ egs/runtsubame.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#$ -cwd                      ## Execute a job in current directory
+#$ -l q_node=1               ## Use number of node
+#$ -l h_rt=01:30:00          ## Running job time
+
+echo "start runtsubame.sh"
+/usr/bin/env
+. /etc/profile.d/modules.sh  ## Initialize module commands
+module load cuda/9.0.176     ## Use GPU
+module load intel
+module load cudnn/7.1
+module load nccl/2.2.13
+module load openmpi/2.1.2-pgi2018
+module matlab
+
+echo "finish load module"
+export PATH="/gs/hs0/tga-tslecture/local/anaconda3/bin:$PATH"
+
+/usr/bin/env
+source activate
+
+echo "activated environment"
+/usr/bin/env
+export PYTHONPATH=     ##remove PYTHONPATH if it have some path
+
+echo "run ./run.sh"
+basch ./run.bash test 10
